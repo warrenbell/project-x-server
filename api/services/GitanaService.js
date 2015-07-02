2bc8d64d08e61595dc1cae16ea03c61039e365c7
warrenbell <warrenbell2@gmail.com>
2015-07-01 19:18:56 -0700

    Added comments

--- api/services/GitanaService.js
+++ api/services/GitanaService.js
@@ -74,6 +74,20 @@ Example of gitanaConfig for appUser connection
 
 module.exports = function() {
 
+
+  /*
+    gitanaConfig should look like this
+
+    {
+      "clientKey": "5d6d1290-1f9b-44fc-864d-1b743e528a55",
+      "clientSecret": "oeafPRBaN0LUxXfcZre/GjMN2gcW+Y7UAuGkUID1B8hCKPQJhFQpEiBsn7BBkZ6bA1pqdRZHjdtaZtLLeagtCbR9iua8DxEgilPwcL7nbrg=",
+      "username": "6c12eb79-cda1-41db-abb0-891231b2cbac",
+      "password": "cT2vrg9T+s8eKpVTC5KWbgUNEoz8/adKnVMQGgURkjx3WXBUTBkVRDlktX5PFWnSFP2g3OTApwo0Mk3kyn4ez6GE4QeiButBO/pxjAmrly0=",
+      "baseURL": "https://api.cloudcms.com",
+      "application": "9f923e9ef3e69ccaa64f"
+    }
+  */
+
   var doConnectAppUser = function(gitanaConfig, callback) {
     sails.log.info("gitanaConfig=" + JSON.stringify(gitanaConfig, null, 2));
 
@@ -93,6 +107,24 @@ module.exports = function() {
       callback.call(this);
     });
   };
+
+  /*
+    gitanaConfig should look like this
+
+    {
+      "clientKey": "5d6d1290-1f9b-44fc-864d-1b743e528a55",
+      "clientSecret": "oeafPRBaN0LUxXfcZre/GjMN2gcW+Y7UAuGkUID1B8hCKPQJhFQpEiBsn7BBkZ6bA1pqdRZHjdtaZtLLeagtCbR9iua8DxEgilPwcL7nbrg=",
+      "baseURL": "https://api.cloudcms.com",
+      "application": "9f923e9ef3e69ccaa64f"
+    }
+
+    usernamePasswordSecret should look like this
+
+    {
+      "username": "username",
+      "password": "password"
+    }
+  */
 
   var doConnectWithAuthentication = function(usernamePasswordSecret, gitanaConfig, req, callback) {
     sails.log.info("gitanaConfig=" + JSON.stringify(gitanaConfig, null, 2));
@@ -122,13 +154,26 @@ module.exports = function() {
 
       callback.call(this, err);
     }).then(function() {
-      // Authentication succeded handler function
-      sails.log.info("Login Succeded");
+      // Authentication succeeded handler function
+      sails.log.info("Login Succeeded");
 
       req.session.accessToken = this.getDriver().http.accessToken();
       callback.call(this);
     });
   };
+
+  /*
+    gitanaConfig should look like this
+
+    {
+      "clientKey": "5d6d1290-1f9b-44fc-864d-1b743e528a55",
+      "clientSecret": "oeafPRBaN0LUxXfcZre/GjMN2gcW+Y7UAuGkUID1B8hCKPQJhFQpEiBsn7BBkZ6bA1pqdRZHjdtaZtLLeagtCbR9iua8DxEgilPwcL7nbrg=",
+      "baseURL": "https://api.cloudcms.com",
+      "application": "9f923e9ef3e69ccaa64f",
+      "token": "45c9c3bc-f1d0-4ab7-8ff8-053f6e877bd0"
+    }
+
+  */
 
   var doConnectWithAccessToken = function(gitanaConfig, req, callback) {
     sails.log.info("gitanaConfig=" + JSON.stringify(gitanaConfig, null, 2));
@@ -146,13 +191,26 @@ module.exports = function() {
         callback.call(this, err);
       }
     }).then(function() {
-      // Connection with access token succeded handler function
-      sails.log.info("Connection with access token Succeded");
+      // Connection with access token succeeded handler function
+      sails.log.info("Connection with access token Succeeded");
 
       req.session.accessToken = this.getDriver().http.accessToken();
       callback.call(this);
     });
   };
+
+  /*
+    gitanaConfig should look like this
+
+    {
+      "clientKey": "5d6d1290-1f9b-44fc-864d-1b743e528a55",
+      "clientSecret": "oeafPRBaN0LUxXfcZre/GjMN2gcW+Y7UAuGkUID1B8hCKPQJhFQpEiBsn7BBkZ6bA1pqdRZHjdtaZtLLeagtCbR9iua8DxEgilPwcL7nbrg=",
+      "baseURL": "https://api.cloudcms.com",
+      "application": "9f923e9ef3e69ccaa64f",
+      "token": "45c9c3bc-f1d0-4ab7-8ff8-053f6e877bd0"
+    }
+
+  */
 
   var doDisconnect = function(gitanaConfig, req, callback) {
     sails.log.info("gitanaConfig=" + JSON.stringify(gitanaConfig, null, 2));
@@ -170,8 +228,8 @@ module.exports = function() {
         callback.call(this, err);
       }
     }).logout().then(function() {
-      // Connection with access token succeded handler function
-      sails.log.info("Disconnecting with access token Succeded");
+      // Connection with access token succeeded handler function
+      sails.log.info("Disconnecting with access token Succeeded");
       req.session.accessToken = undefined;
       callback.call(this);
     });
